a70ca6d3641d9e8f1236d5a365e54506c3a77b67
Songshilong <Songshilongi@users.noreply.github.com>
2024-07-18 10:20:50 +0800

    203. 移除链表元素

--- src/main/java/com/songshilong/linkedList/Solution203.java
+++ src/main/java/com/songshilong/linkedList/Solution203.java
@@ -3,8 +3,22 @@ package com.songshilong.linkedList;
 public class Solution203 {
 
 
-
-
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode currentNode = head;
+        ListNode preNode = dummy;
+        while (currentNode != null) {
+            if (currentNode.val == val) {
+                preNode.next = currentNode.next;
+            }else {
+                preNode = currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return dummy.next;
+    }
 
 
 }
