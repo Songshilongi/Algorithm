397b86097c59a587b899016cdc01fbb875e4daa0
Songshilong <Songshilongi@users.noreply.github.com>
2024-08-03 18:56:35 +0800

    94. 二叉树的中序遍历--迭代法

---
+++ src/main/java/com/songshilong/binarytree/Solution94.java
@@ -0,0 +1,28 @@
+package com.songshilong.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution94 {
+
+    /**
+     * 94. 二叉树的中序遍历--迭代法
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        back(root, result);
+        return result;
+    }
+
+    public void back(TreeNode root, List<Integer> result){
+        if (root == null) return;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        back(left, result);
+        result.add(root.val);
+        back(right, result);
+    }
+
+
+}
