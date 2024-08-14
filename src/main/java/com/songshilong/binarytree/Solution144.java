3bd2e5680b41181052a6cbe0281799ad7ee5a999
Songshilong <Songshilongi@users.noreply.github.com>
2024-08-14 20:08:25 +0800

    前序遍历-迭代法

--- src/main/java/com/songshilong/binarytree/Solution144.java
+++ src/main/java/com/songshilong/binarytree/Solution144.java
@@ -2,6 +2,7 @@ package com.songshilong.binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution144 {
 
@@ -26,13 +27,26 @@ public class Solution144 {
     }
 
 
-
-
-
-
-
-
-
+    /**
+     * 迭代法
+     */
+    public List<Integer> preorderTraversalIteration(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
 
 
 }
