f4428d9a0a3ec66e66843b5172cbba11f36862d3
Songshilong <Songshilongi@users.noreply.github.com>
2024-08-31 15:45:29 +0800

    feat: 110.平衡二叉树

---
+++ src/main/java/com/songshilong/binarytree/Solution110.java
@@ -0,0 +1,47 @@
+package com.songshilong.binarytree;
+
+import java.lang.foreign.AddressLayout;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution110 {
+
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return eval(root.left, root.right) && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public boolean eval(TreeNode left, TreeNode right) {
+        return getDepth(left) - getDepth(right) == 0 ||
+                getDepth(left) - getDepth(right) == 1 ||
+                getDepth(left) - getDepth(right) == -1 ||
+                getDepth(right) - getDepth(left) == 1 ||
+                getDepth(right) - getDepth(left) == -1;
+    }
+
+
+    public int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                size--;
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+
+
+}
