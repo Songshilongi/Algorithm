df42223e0bdbef8de3fb20c330ae629b90e361a0
Songshilong <Songshilongi@users.noreply.github.com>
2024-09-05 21:52:10 +0800

    feat: 513. 找树左下角的值

---
+++ src/main/java/com/songshilong/binarytree/Solution513.java
@@ -0,0 +1,29 @@
+package com.songshilong.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution513 {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            result = queue.peek().val;
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                size--;
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return result;
+    }
+
+
+}
