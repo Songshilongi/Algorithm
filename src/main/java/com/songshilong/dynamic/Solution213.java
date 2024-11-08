f2fef849fbe472d78a9febcc5e0db4cc2bb340a4
ives.song <ives.song@1data.info>
2024-11-08 22:36:04 +0800

    feat: 213. 打家劫舍 II

---
+++ src/main/java/com/songshilong/dynamic/Solution213.java
@@ -0,0 +1,32 @@
+package com.songshilong.dynamic;
+
+/**
+ * @BelongsProject: Algorithm
+ * @BelongsPackage: com.songshilong.dynamic
+ * @Author: Song shi long
+ * @CreateTime: 2024-11-08  22:24
+ * @Description: 213. 打家劫舍 II
+ * @Version: 1.0
+ */
+public class Solution213 {
+
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(dynamic(nums, 0, nums.length - 2), dynamic(nums, 1, nums.length - 1));
+    }
+
+
+    public int dynamic(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[end];
+    }
+}
