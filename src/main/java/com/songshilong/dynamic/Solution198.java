b984f82578a7e36679e8eed896cf8b11dd48facb
ives.song <ives.song@1data.info>
2024-11-08 22:00:06 +0800

    feat: 198. 打家劫舍

---
+++ src/main/java/com/songshilong/dynamic/Solution198.java
@@ -0,0 +1,32 @@
+package com.songshilong.dynamic;
+
+/**
+ * @BelongsProject: Algorithm
+ * @BelongsPackage: com.songshilong.dynamic
+ * @Author: Song shi long
+ * @CreateTime: 2024-11-08  21:46
+ * @Description: 198. 打家劫舍
+ * @Version: 1.0
+ */
+public class Solution198 {
+
+
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        } else if (len == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[len - 1];
+    }
+
+
+
+}
