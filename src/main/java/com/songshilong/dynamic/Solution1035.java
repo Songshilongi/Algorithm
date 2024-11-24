2040c21eb0e882d10a0c02f03af011100ccc751b
ives.song <ives.song@1data.info>
2024-11-24 18:55:06 +0800

    feat: 1035. 不相交的线

---
+++ src/main/java/com/songshilong/dynamic/Solution1035.java
@@ -0,0 +1,34 @@
+package com.songshilong.dynamic;
+
+/**
+ * @BelongsProject: Algorithm
+ * @BelongsPackage: com.songshilong.dynamic
+ * @Author: Song shi long
+ * @CreateTime: 2024-11-24  18:38
+ * @Description: 1035. 不相交的线
+ * @Version: 1.0
+ */
+public class Solution1035 {
+
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+
+        for (int i = 1; i < len1 + 1; i++) {
+            for (int j = 1; j < len2 + 1; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+
+    }
+
+
+}
